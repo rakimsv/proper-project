6f50cd2cefa8246f3d07dc52f56a3c3647155da6
Rakim <rakimsinghverma@gmail.com>
2019-11-20 15:33:41 +0000

    changed server port to 80

---
+++ target/classes/static/precache-manifest.8bfcba9a7c304e5185292e2af9a38a7d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5bfe59e9de276cf1800b291e8edff6f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a1f6bc3d40f65810992f",
+    "url": "/static/css/2.a2750799.chunk.css"
+  },
+  {
+    "revision": "2209164cf116200f0a27",
+    "url": "/static/css/main.f3cbe813.chunk.css"
+  },
+  {
+    "revision": "a1f6bc3d40f65810992f",
+    "url": "/static/js/2.df5c0fc0.chunk.js"
+  },
+  {
+    "revision": "2209164cf116200f0a27",
+    "url": "/static/js/main.ee00ed77.chunk.js"
+  },
+  {
+    "revision": "66ec99c66fc63932591b",
+    "url": "/static/js/runtime-main.0cc5ba3c.js"
+  }
+]);
